173aab0738663f68935103557b8461ae990f52e5
Хмелинина Ольга <olgahmelinina319@gmail.com>
2025-09-03 00:14:35 +0600

    отформатировано

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,11 @@
 from masks import get_mask_account, get_mask_card_number
 from datetime import datetime
 
+
 def mask_account_card(card_info: str) -> str:
     """Функция принимает один аргумент — строку, содержащую тип и номер карты
     возвращает строку с замаскированным номером."""
-    card_type, card_number = card_info.rsplit( sep=" ", maxsplit=1)
+    card_type, card_number = card_info.rsplit(sep=" ", maxsplit=1)
 
     if card_type in ["Visa Platinum", "Maestro"]:
         if len(card_number) == 16:
@@ -15,6 +16,7 @@ def mask_account_card(card_info: str) -> str:
     else:
         return "Некорректный ввод"
 
+
 def get_date(date_str: str) -> str:
     try:
         date_obj = datetime.fromisoformat(date_str)
@@ -22,6 +24,7 @@ def get_date(date_str: str) -> str:
         return formatted_date
     except ValueError:
         return "Некорректный формат даты"
+
 
 if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 690224245640522800"))
